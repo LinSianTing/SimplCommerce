e88c97cd8a34819c533c8949945aa3a2e9ec907e
LinSianTing <siantingtaiwan@gmail.com>
2019-11-08 09:47:05 +0800

    練習新增一組 Module

---
+++ src/Modules/SimplCommerce.Module.TestModuleEric01/ModuleInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using SimplCommerce.Infrastructure.Modules;
+
+namespace SimplCommerce.Module.TestModuleEric01
+{
+    public class ModuleInitializer : IModuleInitializer
+    {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
