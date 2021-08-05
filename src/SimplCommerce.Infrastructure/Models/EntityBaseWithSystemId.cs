7a6634ff5aa2a94d1f167d75ec689d15e1ab284a
linsianting <siantingtaiwan@gmail.com>
2021-08-06 00:33:13 +0800

    於基底類別 EntityBase 增加 繼承 SystemId 特性 Provide a SystemId Column to support mutiple system design Added by Lin SianTing at 20210805
    SystemId = table.Column<int>(nullable: true),

---
+++ src/SimplCommerce.Infrastructure/Models/EntityBaseWithSystemId.cs
@@ -0,0 +1,12 @@
+﻿namespace SimplCommerce.Infrastructure.Models
+{
+    /// <summary>
+    /// Provide a SystemId Column to support mutiple system design
+    /// Added by Lin SianTing at 20210805
+    /// </summary>
+    /// <typeparam name="TId"></typeparam>
+    public abstract class EntityBaseWithSystemId<TId> : IEntityWithSystemId<TId>
+    {
+        public virtual TId SystemId { get; protected set; }
+    }
+}
---
+++ src/SimplCommerce.Infrastructure/Models/IEntityWithSystemId.cs
@@ -0,0 +1,12 @@
+﻿namespace SimplCommerce.Infrastructure.Models
+{
+    /// <summary>
+    /// Provide a SystemId Column to support mutiple system design
+    /// Added by Lin SianTing at 20210805
+    /// </summary>
+    /// <typeparam name="TId"></typeparam>
+    public interface IEntityWithSystemId<TId>
+    {
+        TId SystemId { get; }
+    }
+}
